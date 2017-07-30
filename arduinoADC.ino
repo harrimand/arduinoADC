2825710000ffd34d1b1086ecb3685e92993c47f0
harrimand <harrimand@gmail.com>
2017-07-29 20:08:21 -0700

    Update arduinoADC.ino

--- arduinoADC.ino
+++ arduinoADC.ino
@@ -9,18 +9,27 @@ void setup() {
 #ifdef debugMode
   Serial.begin(9600);
 #endif
+  //Port/Pin Input/Output Configuration
   DDRB = 0x0F;  //PORTB 3..0 Pins 11..8 Output for upper 4 bits of data to LEDs
   DDRD = 0xF0;  //PORTD 7..4 Pins 7..4 Output for lower 4 bits of data to LEDs
   DDRC = (1<<PC2)|(0<<PC1)|(1<<PC0);  //PORTC 2 = Pot Ground, 1 = Analog In, 0 = Pot Vcc
   PORTC = (0<<PC2)|(0<<PC1)|(1<<PC0); //Output 5 V to Pot Vcc
+  
+  //Timer 2 configuration  32KHz Overflow Interrupt.  No PWM Output  
   OCR2A = 250;        //Timer 2 Top Value; Timer Counts 0 -> 250 -> 0 
   TIMSK2 = (1<<TOIE2);  //Timer 2 Overflow Interrupt when Timer = 0
   TCCR2A = (0<<WGM21)|(1<<WGM20); //Waveform Generation Mode 5 (Phase/Frequency Correct PWM)
   TCCR2B = (1<<WGM22)|(0<<CS22)|(0<<CS21)|(1<<CS20);  //Not outputting PWM.  Clock Select=F_cpu/1
+
+  //Analog to Digital Converter (ADC) configuration
+  //Converstion time with this configuration is 104 microSeconds
+  //Exceeding 9615 conversions per second will result in missed readings.
+  //This example is triggering 50 conversions per second.
   ADMUX = (1<<REFS0)|(1<<ADLAR)|(1<<MUX0);  //Analog Reference = 5V. Using 8 bit result. Channel 1
   PRR = PRR & 0xEE;   //Power Reduction Register Analog to Digital Converter turned on.
   //Configure Analog to digital Converter.  ADC enabled, AutoTrigger off, ADC clock = F_cpu/128 
   ADCSRA = (1<<ADEN)|(0<<ADSC)|(0<<ADATE)|(1<<ADIF)|(0<<ADIE)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
+  
 #ifdef debugmode
   Serial.print("ADMUX = ");
   Serial.print(ADMUX, HEX);
